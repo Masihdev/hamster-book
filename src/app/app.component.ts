9b2f5c85c8c47d6d3d386d7e3d144010da51dd12
Masihdev <masihullahmassudi85@gmail.com>
2022-11-14 17:22:32 +0100

    new arrays are made for texts and pics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'hamster-book';
+  postTexts: string[] = [
+    'Hallo, ich bin Frederick. Ich bin hier, um neue Freunde zu treffen!',
+    'Ich will in den Wald gehen',
+    'My Freunde sind nett',
+    'Ich habe viele Geschwister',
+  ];
+
+  postImages: string[] = ['1.jpg', '2.jpg', '3.jpg', '4.jpg'];
 }
